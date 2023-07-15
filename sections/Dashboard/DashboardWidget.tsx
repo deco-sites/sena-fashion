43f6abd51ff5fa5f624e7f2067ca542022db22d3
Victor Sena Molero <victorsenam@gmail.com>
2023-07-15 15:23:26 +0100

    [wip] Unsuccessful d3 timeseries

--- sections/Dashboard/DashboardWidget.tsx
+++ sections/Dashboard/DashboardWidget.tsx
@@ -1,7 +1,15 @@
 import type { LoaderReturnType } from "$live/types.ts";
+import * as d3 from "https://cdn.skypack.dev/d3?dts";
+import {
+  apply,
+  flatten,
+  map,
+  max,
+  min,
+} from "https://cdn.skypack.dev/ramda?dts";
 
 export interface Point {
-  x: number | Date;
+  x: Date;
   y: number;
 }
 
@@ -21,24 +29,51 @@ export interface Props {
   graphData: LoaderReturnType<GraphData>;
 }
 
+const WIDTH = 300;
+
 export default function DashboardWidget(
   { graphData: { name, series } }: Props,
 ) {
+  const dates: Array<Date> = flatten(
+    map((s) => s.points.map((point) => point.x), series),
+  );
+  const start: Date = dates.reduce((acc, curr) => min(acc, curr), new Date());
+  const end: Date = dates.reduce((acc, curr) => max(acc, curr), new Date());
+
+  const xScale = d3.scaleTime().domain([start, end]).range([0, WIDTH]);
+  const yScale = d3.scaleLinear().domain([0, 100]).range([WIDTH, 0]);
+  const lineBuilder = d3.line()
+    .x(([x, y]) => xScale(x))
+    .y(([x, y]) => yScale(y));
+  const linePaths = series.map(({ points }) =>
+    lineBuilder(points.map(({ x, y }) => [x.getDate(), y]))
+  ).filter(Boolean);
+
   return (
     <div>
       <h1>{name}</h1>
-      {series.map(({ label, points }) => (
-        <div>
-          <h2>{label}</h2>
-          <ul>
-            {points.map(({ x, y }) => (
-              <li>
-                <b>{x}</b> {y}
-              </li>
-            ))}
-          </ul>
-        </div>
-      ))}
+      <div>
+        <svg width={WIDTH} height={WIDTH}>
+          <g
+            width={WIDTH}
+            height={WIDTH}
+          >
+            {linePaths.map((linePath) => {
+              if (linePath == null) {
+                return null;
+              }
+              return (
+                <path
+                  d={linePath}
+                  stroke="#666"
+                  fill="none"
+                  strokeWidth={2}
+                />
+              );
+            })}
+          </g>
+        </svg>
+      </div>
     </div>
   );
 }
